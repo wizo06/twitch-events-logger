4f17c753476ebe6c85e90926f093939786fb8937
wizo06 <14183421+wizo06@users.noreply.github.com>
2022-06-02 01:26:24 -0400

    asdf

--- src/index.js
+++ src/index.js
@@ -5,30 +5,121 @@ const { Timestamp } = require("firebase-admin/firestore");
 
 const chatClient = new ChatClient({ channels: ["umikoyui"] });
 
+const formatTimestamp = (x) => {
+  const d = new Date(x);
+  const month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : d.getMonth() + 1;
+  const date = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
+  const hour = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
+  const min = d.getMinutes < 10 ? `0${d.getMinutes()}` : d.getMinutes();
+  const sec = d.getSeconds < 10 ? `0${d.getSeconds()}` : d.getSeconds();
+  const timestamp = `${d.getFullYear()}.${month}.${date}|${hour}:${min}:${sec}`;
+
+  return timestamp;
+};
+
+const getUserId = async (u) => {
+  const userSnapshot = await db.collection("users").where("name", "==", u).get();
+  if (userSnapshot.empty) {
+    const user = await apiClient.users.getUserByName(u);
+    await db.collection("users").doc(user.id).set({
+      name: user.name,
+      creationDate: user.creationDate,
+      displayName: user.displayName,
+      profilePictureUrl: user.profilePictureUrl,
+    });
+    return user.id;
+  }
+
+  return userSnapshot.docs[0].id;
+};
+
 (async () => {
   await chatClient.connect();
 
-  chatClient.onMessage(async (ch, u, message, msg) => {
-    const d = new Date(msg.date);
-    const month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : d.getMonth() + 1;
-    const date = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
-    const hour = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
-    const min = d.getMinutes < 10 ? `0${d.getMinutes()}` : d.getMinutes();
-    const sec = d.getSeconds < 10 ? `0${d.getSeconds()}` : d.getSeconds();
-    const timestamp = `${d.getFullYear()}.${month}.${date}|${hour}:${min}:${sec}`;
+  chatClient.onAction(async (ch, u, message, msg) => {
+    const timestamp = formatTimestamp(msg.date);
 
-    const user = await apiClient.users.getUserByName(u);
-    const channel = await apiClient.users.getUserByName(ch.replace("#", ""));
+    const userId = await getUserId(u);
+    const channelId = await getUserId(ch.replace("#", ""));
 
-    console.log(`${timestamp} ${user.name}: ${message}`);
+    console.log(`${timestamp} ${u}: ${message}`);
 
-    db.collection("message")
+    await db
+      .collection("action")
       .doc(msg.id)
       .set({
-        timestamp: Timestamp.fromDate(d),
-        userId: user.id,
-        channelId: channel.id,
+        timestamp: Timestamp.fromDate(msg.date),
+        userId,
+        channelId,
         message: message,
       });
   });
+
+  // Not implemented yet
+  // chatClient.onAnnouncement(async (ch, u, announcementInfo, msg) => {
+  //   const timestamp = formatTimestamp(msg.date);
+  //   const userId = await getUserId(u);
+  //   const channelId = await getUserId(ch.replace("#", ""));
+  //   console.log(`${timestamp} ${u}: ${msg.message}`);
+  //   console.log(announcementInfo);
+  // })
+
+  // Not fully implemented yet
+  chatClient.onBan(async (ch, u, msg) => {
+    const timestamp = formatTimestamp(msg.date);
+
+    const userId = await getUserId(u);
+    const channelId = await getUserId(ch.replace("#", ""));
+
+    console.log(`${timestamp} ${u}: ${msg}`);
+    console.log(msg);
+
+    await db
+      .collection("ban")
+      .doc()
+      .set({
+        timestamp: Timestamp.fromDate(msg.date),
+        userId,
+        channelId,
+      });
+  });
+
+  chatClient.onMessage(async (ch, u, message, msg) => {
+    const timestamp = formatTimestamp(msg.date);
+
+    const userId = await getUserId(u);
+    const channelId = await getUserId(ch.replace("#", ""));
+
+    console.log(`${timestamp} ${u}: ${message}`);
+
+    await db
+      .collection("message")
+      .doc(msg.id)
+      .set({
+        timestamp: Timestamp.fromDate(msg.date),
+        userId,
+        channelId,
+        message: message,
+      });
+  });
+
+  // Not fully implemented yet
+  chatClient.onTimeout(async (ch, u, duration, msg) => {
+    const timestamp = formatTimestamp(msg.date);
+
+    const userId = await getUserId(u);
+    const channelId = await getUserId(ch.replace("#", ""));
+
+    console.log(`${timestamp} ${u}: ${duration} ${msg}`);
+    console.log(msg);
+
+    await db
+    .collection("timeout")
+    .doc()
+    .set({
+      timestamp: Timestamp.fromDate(msg.date),
+      userId,
+      channelId,
+    });
+  })
 })();
